46ce3e03a03cbf7c392fcaf71eb75f980ccf7de1
sunie <1287495769@qq.com>
2023-04-03 10:17:10 +0800

    add all

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
+import eventlet
+eventlet.monkey_patch()     # 使用eventlet而不是greenlet启动socketio
+
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO, emit
+from sqlalchemy.orm import scoped_session, sessionmaker
+from datetime import datetime
+import json
+import queue
+import time
+import threading
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///editable_table.db'
 db = SQLAlchemy(app)
+task_queue = queue.Queue()
+
+# 创建一个新的Session工厂
+Session = scoped_session(sessionmaker(bind=db.engine))
 
 class TableRow(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,12 +26,56 @@ class TableRow(db.Model):
     user = db.Column(db.String(120), nullable=True)
     department = db.Column(db.String(120), nullable=True)
     target = db.Column(db.String(120), nullable=False)
-    create_time = db.Column(db.DateTime)
-    update_time = db.Column(db.DateTime)
+    create_time = db.Column(db.DateTime, default=datetime.now())
+    update_time = db.Column(db.DateTime, default=datetime.now())
     status = db.Column(db.String(120), nullable=False)
+
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
 db.create_all()
+socketio = SocketIO(app, cors_allowed_origins="*")
+# 创建一个字典来存储每个worker的状态
+worker_count = 3
+worker_idle = worker_count
+
+def worker():
+    global worker_idle
+    while True:
+        data = task_queue.get()
+        if data is None:
+            break
+        worker_idle -= 1
+
+        # 创建一个新的Session
+        session = Session()
+
+        # 将原始对象与新Session关联
+        table_row = session.merge(data)
+
+        print(table_row, table_row.id, table_row.source, table_row.status)
+        # 模拟一个耗时任务
+        time.sleep(5)
+        print({'row_id': table_row.id, 'status': 'uploading'})
+        socketio.emit('status_change', {'row_id': table_row.id, 'status': 'uploading'}, namespace='/table')
+        
+        time.sleep(5)
+        print({'row_id': table_row.id, 'status': 'done'})
+        socketio.emit('status_change', {'row_id': table_row.id, 'status': 'done'}, namespace='/table')
+        table_row.status = 'done'
+        table_row.update_time = datetime.now()
+        session.commit()
+        worker_idle += 1
+
+
+# 创建三个worker线程
+worker_threads = [threading.Thread(target=worker) for _ in range(3)]
+
+# 启动worker线程
+for thread in worker_threads:
+    thread.start()
+
 
 @app.route('/')
 def index():
@@ -26,7 +84,8 @@ def index():
 @app.route('/get-data', methods=['GET'])
 def get_data():
     table_rows = TableRow.query.all()
-    return jsonify(table_rows)
+    table_rows_dict = [row.to_dict() for row in table_rows]
+    return jsonify(table_rows_dict)
 
 @app.route('/update-row', methods=['POST'])
 def update_cell():
@@ -42,12 +101,18 @@ def update_cell():
 @app.route('/create-row', methods=['POST'])
 def create_row():
     row_id = request.form.get('row_id')
-    content = request.form.get('new_content')
-    new_row = TableRow(id=row_id, content=content)
+    content = request.form.get('content')
+    if content:
+        content = json.loads(content)
+    print('create:', row_id, content)
+    status = 'pulling' if worker_idle else 'waiting'
+    new_row = TableRow(id=row_id, source=content[0], frequency=content[1], user=content[2], \
+        department=content[3], target=content[4], status=status)
     db.session.add(new_row)
     db.session.commit()
-    return jsonify({"status": "success", "row_id": new_row.id})
+    task_queue.put(new_row)
+    return jsonify({"status": "success"})
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    socketio.run(app, debug=True, port=8000, host='0.0.0.0')
--- static/main.js
+++ static/main.js
@@ -1,3 +1,20 @@
+// 连接WebSocket
+var protocol = location.protocol === 'https:' ? 'wss://' : 'ws://';
+var namespace = '/table';
+var socket = io.connect(protocol + window.location.hostname + ':' + location.port + namespace);
+
+// 监听cell_update事件
+socket.on('status_change', function(data) {
+  // 根据接收到的数据更新表格
+  console.log('status change', data);
+  // 选择具有data-id为5的tr元素
+  var trElement = $(`tr[data-id="${data.row_id}"]`);
+  // 选择tr下的第六个td元素，即status（注意：eq()方法的索引从0开始）
+  var tdElement = trElement.find('td:eq(5)');
+  // 将第六个td元素的内容更改为"done"
+  tdElement.text(data.status);
+});
+
 function renderTable(data) {
     const table = $("#editable-table");
     // 清空表格内容
@@ -7,10 +24,17 @@ function renderTable(data) {
         if (row.id > max_id) max_id = row.id;
         const tr = $("<tr></tr>").attr("data-id", row.id);
         // 填充表格行的五列
-        for (let i = 0; i < 5; i++) {
-            const td = $("<td contenteditable='false'></td>").text(row.content);
+        function add_contend(content) {
+            const td = $("<td contenteditable='false'></td>").text(content);
             tr.append(td);
         }
+        add_contend(row.source);
+        add_contend(row.frequency);
+        add_contend(row.user);
+        add_contend(row.department);
+        add_contend(row.target);
+        add_contend(row.status);
+
         // 添加“编辑/提交”按钮
         const button = $("<button></button>").text("编辑");
         button.on("click", function() {
@@ -22,13 +46,13 @@ function renderTable(data) {
 
     // 添加一个空行作为最后一行
     const tr = $("<tr></tr>").attr("data-id", max_id+1);
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 6; i++) {
         const td = $("<td contenteditable='true'></td>").text('');
         tr.append(td);
     }
     const button = $("<button></button>").text("添加");
     button.on("click", function() {
-        createRow(tr, button);
+        createRow(tr);
     });
     tr.append($("<td></td>").append(button));
     table.append(tr);
@@ -39,6 +63,7 @@ function getData() {
         url: '/get-data',
         method: 'GET',
         success: function (data) {
+            console.log(data)
             renderTable(data);
         }
     });
@@ -80,16 +105,24 @@ function toggleEditMode(row, button) {
 function createRow(row) {
     // 你需要在后端实现一个新的路由以处理这个请求
     const rowId = row.attr("data-id");
-    const rowData = row.find("td").map(function() {
-        return $(this).text();
-    }).get();
+    // const rowData = row.find("td").map(function() {
+    //     return $(this).text();
+    // }).get();
+    let rowData = [];
+    $(row).find('td').each(function() {
+        // 提取文本内容并添加到数组中
+        rowData.push($(this).text());
+      });
+
+    console.log(rowId)
+    console.log(rowData)
 
     $.ajax({
         url: '/create-row',
         method: 'POST',
         data: {
             row_id: rowId,
-            content: rowData
+            content: JSON.stringify(rowData.slice(0, rowData.length-1)) 
         },
         success: function (response) {
             if (response.status === 'success') {
